0499c949672dfad7779ed4b27fd2a3e494254a69
xiudu123 <844506672@qq.com>
2022-11-02 16:20:15 +0800

    配置 MySQL 与注册登录 3

--- backend/src/main/java/com/kob/backend/service/impl/user/account/RegisterServiceImpl.java
+++ backend/src/main/java/com/kob/backend/service/impl/user/account/RegisterServiceImpl.java
@@ -37,6 +37,7 @@ public class RegisterServiceImpl implements RegisterService {
         }
         if(password.length() == 0 || confirmedPassword.length() == 0){
             map.put("error_message", "密码不能为空");
+            return map;
         }
         if(username.length() > 100){
             map.put("error_message", "用户名长度不能大于100");
