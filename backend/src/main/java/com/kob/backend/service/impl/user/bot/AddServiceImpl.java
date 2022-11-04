ed493be89bfbf2b10149ca21f750ef5b882629f8
xiudu123 <844506672@qq.com>
2022-11-05 00:49:32 +0800

    创建个人中心页面 1

---
+++ backend/src/main/java/com/kob/backend/service/impl/user/bot/AddServiceImpl.java
@@ -0,0 +1,69 @@
+package com.kob.backend.service.impl.user.bot;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kob.backend.mapper.BotMapper;
+import com.kob.backend.pojo.Bot;
+import com.kob.backend.pojo.User;
+import com.kob.backend.service.impl.utils.UserDetailsImpl;
+import com.kob.backend.service.user.bot.AddService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class AddServiceImpl implements AddService {
+
+    @Autowired
+    private BotMapper botMapper;
+
+    @Override
+    public Map<String, String> add(Map<String, String> data) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl loginuser = (UserDetailsImpl) authenticationToken.getPrincipal();
+        User user = loginuser.getUser();
+
+        String title = data.get("title");
+        String description = data.get("description");
+        String content = data.get("content");
+        Map<String, String> map = new HashMap<>();
+
+        if(title == null || title.length() == 0){
+            map.put("error_message", "标题不能为空");
+            return map;
+        }
+        if(title.length() > 100){
+            map.put("error_message", "标题过长");
+            return map;
+        }
+
+        if(description == null || description.length() == 0){
+            description = "这个用户很懒，什么也没有留下~";
+        }
+
+        if(description.length() > 300){
+            map.put("error_message", "Bot描述过长");
+            return map;
+        }
+
+        if(content == null || content.length() == 0){
+            map.put("error_message", "代码不能为空");
+            return map;
+        }
+        if(content.length() > 10000){
+            map.put("error_message", "代码过长");
+            return map;
+        }
+
+        Date now = new Date(); // 默认当前时间;
+        Bot bot = new Bot(null, user.getId(), title, description, content, 1500, now, now);
+        botMapper.insert(bot);
+        map.put("error_message", "success");
+        return map;
+    }
+}
--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -6,12 +6,87 @@
 
 <script>
 import ContentField from '@/components/ContentField'
-
+import $ from 'jquery'
+import { useStore } from 'vuex' 
 export default {
     components:{
         ContentField
+    },
+    setup(){
+        const store = useStore();
+        // $.ajax({
+        //     url: 'http://127.0.0.1:3000/user/bot/add/',
+        //     type: 'post',
+        //     data:{
+        //         title: "标题",
+        //         description: "描述",
+        //         content:"代码",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     erroe(resp){
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: 'http://127.0.0.1:3000/user/bot/remove/',
+        //     type: 'post',
+        //     data:{
+        //         bot_id: 5,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     erroe(resp){
+        //         console.log(resp);
+        //     }
+        // })
+
+        // $.ajax({
+        //     url: 'http://127.0.0.1:3000/user/bot/update/',
+        //     type: 'post',
+        //     data:{
+        //         bot_id: 6,
+        //         title: "标题7",
+        //         description: "描述7",
+        //         content:"代码7",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     erroe(resp){
+        //         console.log(resp);
+        //     }
+        // })
+
+        $.ajax({
+            url: 'http://127.0.0.1:3000/user/bot/getlist/',
+            type: 'get',
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp){
+                console.log(resp);
+            },
+            erroe(resp){
+                console.log(resp);
+            }
+        })
     }
 }
+
+
 </script>
 
 <style scoped>
