c699aa613af699f9b9e0a04c21df86551cbc7f0e
xiudu123 <844506672@qq.com>
2022-11-10 18:24:28 +0800

    匹配系统 3

---
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchsystem/service/impl/MatchingServiceImpl.java
@@ -0,0 +1,24 @@
+package com.kob.matchsystem.service.impl;
+
+import com.kob.matchsystem.service.MatchingService;
+import com.kob.matchsystem.service.impl.utils.MatchingPool;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchingServiceImpl implements MatchingService {
+    public final static MatchingPool matchingpool = new MatchingPool();
+
+    @Override
+    public String addPlayer(Integer userId, Integer rating) {
+        System.out.println("add player: " + userId + ' ' + rating);
+        matchingpool.addPlayer(userId, rating);
+        return "add player success";
+    }
+
+    @Override
+    public String removePlayer(Integer userId) {
+        System.out.println("remove player: " + userId);
+        matchingpool.removePlayer(userId);
+        return "remove player success";
+    }
+}
